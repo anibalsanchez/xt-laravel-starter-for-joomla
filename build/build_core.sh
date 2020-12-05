26784fee42e774ff05d0c7913e37e6dd379bb8f6
anibalsanchez <anibal.sanchez@extly.com>
2020-12-05 18:28:04 +0100

    Clean files & organize

--- build/build_core.sh
+++ build/build_core.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-cd library
+# Install the core build scripts
 npm install
+
+# Install Composer Libs into library/vendor
 composer update --no-dev
+
+cd library
+
+# Install the Library's own build scripts
+npm install
+
+# Run the Library's build scripts
 npm run production
 
-# Don't pack node_modules
+# Remove node_modules to avoid packing them
 rm -rf node_modules
+
 cd -
 
-npm install
+# Finally, build and package the extension
 npm run build
